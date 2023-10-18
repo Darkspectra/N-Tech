4babfeb35f170c640eb4cff0ba82cbc254bf087d
Darkspectra <ftanvirnovo@gmail.com>
2023-10-19 00:50:29 +0600

    added extra Discount layer

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "react-router-dom";
 import Banner from "./Banner";
 import TechFooter from "./TechFooter";
 import Cards from "./Cards";
+import extra from "../assets/extra.png";
 
 const Home = () => {
     const brands = useLoaderData()
@@ -14,6 +15,14 @@ const Home = () => {
                 {
                     brands.map(brand => <Cards key={brand.id} brand={brand}></Cards>)
                 }
+            </div>
+            <div className="flex items-center mx-auto">
+                <div>
+                    <p className="text-4xl font-bold">Do not Miss you chance! <br />Grab Discount now using <br/> <span className="text-lime-500">NSINS</span> promo code!</p>
+                </div>
+                <div>
+                    <img src={extra} alt="" />
+                </div>
             </div>
             <TechFooter></TechFooter>
         </div>
